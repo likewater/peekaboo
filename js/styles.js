e35c381f854e999a581be0282b3e24a82962b906
Blake Womack and Kaya Winn <blake@>
2017-02-14 10:21:54 -0800

    adds base pages with minimal functions

---
+++ js/styles.js
@@ -0,0 +1,22 @@
+// $(document).ready(function() {
+//   $("p").click(function() {
+//     $("img").show();
+//   });
+// });
+
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".showing").slideToggle("fast");
+    $(".hidden").slideToggle(6000);
+  });
+});
+
+//
+//   $("peek").onLoad(function(){
+//     $(fadeIn(slow) {
+//
+//   });
+// });
+// });
+//
+// function fadeIn("peek")
